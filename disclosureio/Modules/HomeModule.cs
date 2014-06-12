f3be0a6ce97b7d4844c1fe844687c6e93135b745
Michael West <git@ub3rk1tten.com>
2014-06-12 01:20:52 -0500

    WELP, looks like MVC is too much for mono without lots of hacks. Switching to Nancy...

---
+++ disclosureio/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace disclosureio.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["Index"];
+            };
+        }
+    }
+}
--- disclosureio/Properties/AssemblyInfo.cs
+++ disclosureio/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("disclosureio")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2604c4f9-e6dd-4186-add6-3856b292af7b")]
+[assembly: Guid("26d2f50b-69a9-4974-b89b-937ca816393f")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
