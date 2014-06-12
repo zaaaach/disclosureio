806b9d8adf5d7d800c4e4cc27eea5021b67718e6
Michael West <git@ub3rk1tten.com>
2014-06-11 23:46:28 -0500

    Initial project

---
+++ disclosureio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="disclosureio.MvcApplication" Language="C#" %>
---
+++ disclosureio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(disclosureio.Startup))]
+namespace disclosureio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
