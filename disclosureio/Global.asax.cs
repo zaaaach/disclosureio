76aff9e6f6036d016b7fee4978aab9fd3d11468b
Michael West <git@ub3rk1tten.com>
2014-06-12 00:45:44 -0500

    Trying workaround for mono bug

--- disclosureio/Global.asax.cs
+++ disclosureio/Global.asax.cs
@@ -12,6 +12,9 @@ namespace disclosureio
     {
         protected void Application_Start()
         {
+            // https://stackoverflow.com/questions/16922952/system-nullreferenceexception-object-reference-not-set-to-an-instance-of-an-obj
+            HttpEncoder.Current = HttpEncoder.Default;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
